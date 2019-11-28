48e81edbd65ff4d48e336045138772542e88130b
Calum Hand <crh53@bath.ac.uk>
2019-11-28 16:54:31 +0000

    further functionalisation of data loading and changing function names

--- simulated_screed_HMOF.py
+++ simulated_screed_HMOF.py
@@ -3,29 +3,38 @@
 import numpy as np
 import BOGP
 
+########################################################################################################################
 
-def perform_pseudo_experiment(material, true_results):
+
+def data_loader(path_to_file):
+    data_set = np.loadtxt(path_to_file)
+    X, y = data_set[:, :-1], data_set[:, -1]
+    y = y.reshape(-1, 1)
+    n, d = X.shape
+    return X, y, n, d
+
+
+########################################################################################################################
+
+
+def determine_material_value(material, true_results):
     return true_results[material]
 
 
-data_set = np.loadtxt(r'C:\Users\crh53\OneDrive\Desktop\PHD_Experiments\E2_AMI_James\Data\Scaled_HMOF_Data')
-X, y = data_set[:, :-1], data_set[:, -1]
-y_true = y.reshape(-1, 1)
-n, d = X.shape
+########################################################################################################################
 
-STATUS = np.zeros((n, 1))  # status vector
-y_experimental = np.full((n, 1), np.nan)
-
+X, y_true, n, d = data_loader(r'C:\Users\crh53\OneDrive\Desktop\PHD_Experiments\E2_AMI_James\Data\Scaled_HMOF_Data')
 
 top = np.argsort(y_true)[-100:]  # true top 100 to compare sample with
-
+STATUS = np.zeros((n, 1))  # column vector denoting status of material (0=untested, 1=testing, 2=tested)
+y_experimental = np.full((n, 1), np.nan)  # column vector of determined material performances
 
 material_indices = np.random.permutation(n)  # sample  100 at random to start
 nrand = 100
 ntested = nrand
 for i in range(nrand):
     STATUS[material_indices[i]] = 2
-    y_experimental[material_indices[i]] = perform_pseudo_experiment(material_indices[i], y_true)
+    y_experimental[material_indices[i]] = determine_material_value(material_indices[i], y_true)
 
 P = BOGP.prospector(X)
 
@@ -33,7 +42,7 @@ while ntested < 2000:  # lets go!
     P.fit(y_experimental, STATUS)
     ipick, kpick = P.pick_next(STATUS)  # sample next point
     STATUS[ipick, kpick] = 1  # show that we are testing ipick
-    y_experimental[ipick, kpick] = perform_pseudo_experiment(ipick, y_true)  # now lets get the score and update status
+    y_experimental[ipick, kpick] = determine_material_value(ipick, y_true)  # now lets get the score and update status
     STATUS[ipick, kpick] = 2
     ntested = ntested + 1  # count sample and print out current score
     print(ntested)

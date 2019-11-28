689373f4b556f1e645098186fe6bede956f1ff45
Calum Hand <crh53@bath.ac.uk>
2019-11-28 17:07:14 +0000

    cleaning up initial sampling and placing user inputs at top of file

--- simulated_screed_HMOF.py
+++ simulated_screed_HMOF.py
@@ -24,21 +24,23 @@ def determine_material_value(material, true_results):
 ########################################################################################################################
 
 X, y_true, n, d = data_loader(r'C:\Users\crh53\OneDrive\Desktop\PHD_Experiments\E2_AMI_James\Data\Scaled_HMOF_Data')
+num_initial_samples = 100
+max_iterations = 2000
 
 top = np.argsort(y_true)[-100:]  # true top 100 to compare sample with
 STATUS = np.zeros((n, 1))  # column vector denoting status of material (0=untested, 1=testing, 2=tested)
 y_experimental = np.full((n, 1), np.nan)  # column vector of determined material performances
 
-material_indices = np.random.permutation(n)  # sample  100 at random to start
-nrand = 100
-ntested = nrand
-for i in range(nrand):
-    STATUS[material_indices[i]] = 2
-    y_experimental[material_indices[i]] = determine_material_value(material_indices[i], y_true)
+initial_samples = np.random.randint(0, n, num_initial_samples)  # choose n values where each value is a material index
+for sample in initial_samples:
+    STATUS[sample] = 2
+    y_experimental[sample] = determine_material_value(sample, y_true)  # determine material value and update
+
+ntested = num_initial_samples
 
 P = BOGP.prospector(X)
 
-while ntested < 2000:  # lets go!
+while ntested < max_iterations:  # lets go!
     P.fit(y_experimental, STATUS)
     ipick, kpick = P.pick_next(STATUS)  # sample next point
     STATUS[ipick, kpick] = 1  # show that we are testing ipick

ef75f2292f91502da99a403a7b462aadbda7bc71
James Hook <james.l.hook@gmail.com>
2019-11-28 13:09:37 +0000

    update from nov 28

--- BOGP.py
+++ BOGP.py
@@ -18,67 +18,89 @@ import GPy
 
 class prospector(object):
     
-    """ fits hyperparameters and inducing points """
-    def fit(self,X,untested,tested,ytested,ntop=100,nrecent=100,nmax=400,ntopmu=100,ntopvar=100,nkmeans=300,nkeamnsdata=5000,lam=1e-6):
-        """ fit a GPy dense model to get hyperparameters """
-        """ take subsample for tested data for fitting """
-        """ ntop top samples """
-        """ nrecent most recent samples """
-        """ random samples up to total of nmax """
+    """ Initializes by storing all feature values """ 
+    def __init__(self,X):
+        self.X=X
         self.n,self.d=X.shape
-        print('fitting hyperparameters')
-        ntested=len(tested)
-        if ntested>nmax:
-            top=list(np.argsort(ytested)[-ntop:])
-            recent=list(range(ntested-nrecent,ntested))
-            topandrecent=list(set(top+recent))
-            rand=list(np.random.choice([i for i in range(ntested) if i not in topandrecent],nmax-len(topandrecent),False))
-            testedtrain=topandrecent+rand
-            ytrain=ytested[testedtrain]
-            train=[tested[i] for i in testedtrain]
+        self.update_counter=0
+        self.updates_per_big_fit=10
+    
+    """ fits hyperparameters and inducing points """
+    def fit(self,Y,STATUS,ntop=100,nrecent=100,nmax=400,ntopmu=100,ntopvar=100,nkmeans=300,nkeamnsdata=5000,lam=1e-6):
+        X=self.X
+        untested=[i for i in range(self.n) if STATUS[i]==0]
+        #testing=[i for i in range(self.n) if STAUS[i]==1]
+        tested=[i for i in range(self.n) if STATUS[i]==2]
+        ytested=Y[tested].reshape(-1)
+        if np.mod(self.update_counter,self.updates_per_big_fit)==0:
+            """ fit a GPy dense model to get hyperparameters """
+            """ take subsample for tested data for fitting """
+            """ ntop top samples """
+            """ nrecent most recent samples """
+            """ random samples up to total of nmax """
+            #self.n,self.d=X.shape
+            print('fitting hyperparameters')
+            ntested=len(tested)
+            if ntested>nmax:
+                top=list(np.argsort(ytested)[-ntop:])
+                recent=list(range(ntested-nrecent,ntested))
+                topandrecent=list(set(top+recent))
+                rand=list(np.random.choice([i for i in range(ntested) if i not in topandrecent],nmax-len(topandrecent),False))
+                testedtrain=topandrecent+rand
+                ytrain=ytested[testedtrain]
+                train=[tested[i] for i in testedtrain]
+            else:
+                train=tested
+                ytrain=ytested
+            """ fit dense GPy model to this data """
+            mfy=GPy.mappings.Constant(input_dim=self.d,output_dim=1)
+            ky = GPy.kern.RBF(self.d,ARD=True,lengthscale=np.ones(self.d))
+            self.GP = GPy.models.GPRegression(X[train],ytrain.reshape(-1,1),kernel=ky,mean_function=mfy)
+            self.GP.optimize('bfgs')
+            self.mu=self.GP.flattened_parameters[0]
+            self.a=self.GP.flattened_parameters[1]
+            self.l=self.GP.flattened_parameters[2]
+            self.b=self.GP.flattened_parameters[3]
+            """ now pick inducing points for sparse model """
+            """ use all the train points as above """
+            """ ntopmu most promising untested points """
+            """ ntopvar most uncertain untested points """
+            """ nkmeans cluster centers from untested data """
+            print('selecting inducing points')
+            self.py=self.GP.predict(X)
+            topmu=[untested[i] for i in np.argsort(self.py[0][untested].reshape(-1))[-ntopmu:]]
+            topvar=[untested[i] for i in np.argsort(self.py[1][untested].reshape(-1))[-ntopvar:]]
+            nystrom=topmu+topvar+train
+            kms=KMeans(n_clusters=nkmeans,max_iter=5).fit(np.divide(X[list(np.random.choice(untested,nkeamnsdata))],self.l)) 
+            self.M=np.vstack((X[nystrom],np.multiply(kms.cluster_centers_,self.l)))
+            """ fit sparse model """
+            """ lam controls jitter in g samlpes """
+            print('fitting sparse model')
+            DXM=euclidean_distances(np.divide(X,self.l),np.divide(self.M,self.l),squared=True)
+            self.SIG_XM=self.a*np.exp(-DXM/2)
+            DMM=euclidean_distances(np.divide(self.M,self.l),np.divide(self.M,self.l),squared=True)
+            self.SIG_MM=self.a*np.exp(-DMM/2)+np.identity(self.M.shape[0])*lam*self.a
+            self.B=self.a+self.b-np.sum(np.multiply(np.linalg.solve(self.SIG_MM,self.SIG_XM.T),self.SIG_XM.T),0)
+            K=np.matmul(self.SIG_XM[tested].T,np.divide(self.SIG_XM[tested],self.B[tested].reshape(-1,1)))
+            self.SIG_MM_pos=self.SIG_MM-K+np.matmul(K,np.linalg.solve(K+self.SIG_MM,K))
+            J=np.matmul(self.SIG_XM[tested].T,np.divide(ytested-self.mu,self.B[tested]))
+            self.mu_M_pos=self.mu+J-np.matmul(K,np.linalg.solve(K+self.SIG_MM,J))
         else:
-            train=tested
-            ytrain=ytested
-        """ fit dense GPy model to this data """
-        mfy=GPy.mappings.Constant(input_dim=self.d,output_dim=1)
-        ky = GPy.kern.RBF(self.d,ARD=True,lengthscale=np.ones(self.d))
-        self.GP = GPy.models.GPRegression(X[train],ytrain.reshape(-1,1),kernel=ky,mean_function=mfy)
-        self.GP.optimize('bfgs')
-        self.mu=self.GP.flattened_parameters[0]
-        self.a=self.GP.flattened_parameters[1]
-        self.l=self.GP.flattened_parameters[2]
-        self.b=self.GP.flattened_parameters[3]
-        """ now pick inducing points for sparse model """
-        """ use all the train points as above """
-        """ ntopmu most promising untested points """
-        """ ntopvar most uncertain untested points """
-        """ nkmeans cluster centers from untested data """
-        print('selecting inducing points')
-        self.py=self.GP.predict(X)
-        topmu=[untested[i] for i in np.argsort(self.py[0][untested].reshape(-1))[-ntopmu:]]
-        topvar=[untested[i] for i in np.argsort(self.py[1][untested].reshape(-1))[-ntopvar:]]
-        nystrom=topmu+topvar+train
-        kms=KMeans(n_clusters=nkmeans,max_iter=5).fit(np.divide(X[list(np.random.choice(untested,nkeamnsdata))],self.l)) 
-        self.M=np.vstack((X[nystrom],np.multiply(kms.cluster_centers_,self.l)))
-        """ fit sparse model """
-        """ lam controls jitter in g samlpes """
-        print('fitting sparse model')
-        DXM=euclidean_distances(np.divide(X,self.l),np.divide(self.M,self.l),squared=True)
-        self.SIG_XM=self.a*np.exp(-DXM/2)
-        DMM=euclidean_distances(np.divide(self.M,self.l),np.divide(self.M,self.l),squared=True)
-        self.SIG_MM=self.a*np.exp(-DMM/2)+np.identity(self.M.shape[0])*lam*self.a
-        self.B=self.a+self.b-np.sum(np.multiply(np.linalg.solve(self.SIG_MM,self.SIG_XM.T),self.SIG_XM.T),0)
-        K=np.matmul(self.SIG_XM[tested].T,np.divide(self.SIG_XM[tested],self.B[tested].reshape(-1,1)))
-        self.SIG_MM_pos=self.SIG_MM-K+np.matmul(K,np.linalg.solve(K+self.SIG_MM,K))
-        J=np.matmul(self.SIG_XM[tested].T,np.divide(ytested-self.mu,self.B[tested]))
-        self.mu_M_pos=self.mu+J-np.matmul(K,np.linalg.solve(K+self.SIG_MM,J))
-
-    """ updates posterior on inducing points """
-    def update_model(self,tested,ytested):
-        K=np.matmul(self.SIG_XM[tested].T,np.divide(self.SIG_XM[tested],self.B[tested].reshape(-1,1)))
-        self.SIG_MM_pos=self.SIG_MM-K+np.matmul(K,np.linalg.solve(K+self.SIG_MM,K))
-        J=np.matmul(self.SIG_XM[tested].T,np.divide(ytested-self.mu,self.B[tested]))
-        self.mu_M_pos=self.mu+J-np.matmul(K,np.linalg.solve(K+self.SIG_MM,J))
+            K=np.matmul(self.SIG_XM[tested].T,np.divide(self.SIG_XM[tested],self.B[tested].reshape(-1,1)))
+            self.SIG_MM_pos=self.SIG_MM-K+np.matmul(K,np.linalg.solve(K+self.SIG_MM,K))
+            J=np.matmul(self.SIG_XM[tested].T,np.divide(ytested-self.mu,self.B[tested]))
+            self.mu_M_pos=self.mu+J-np.matmul(K,np.linalg.solve(K+self.SIG_MM,J))            
+        self.update_counter+=1
+        
+#    """ updates posterior on inducing points """
+#    def update_model(self,STATUS,Y):
+#        #tested,ytested
+#        tested=[i for i in range(self.n) if STATUS[i]==2]
+#        ytested=Y[tested]
+#        K=np.matmul(self.SIG_XM[tested].T,np.divide(self.SIG_XM[tested],self.B[tested].reshape(-1,1)))
+#        self.SIG_MM_pos=self.SIG_MM-K+np.matmul(K,np.linalg.solve(K+self.SIG_MM,K))
+#        J=np.matmul(self.SIG_XM[tested].T,np.divide(ytested-self.mu,self.B[tested]))
+#        self.mu_M_pos=self.mu+J-np.matmul(K,np.linalg.solve(K+self.SIG_MM,J))
 
     """ get prediction on full dataset """        
     def predict(self):
@@ -93,7 +115,9 @@ class prospector(object):
         return samples_X_pos
 
     """ picks next point to sample """    
-    def pick_next(self,untested,acquisition_function='Thompson',N=100,nysamples=100):
+    def pick_next(self,STATUS,acquisition_function='Thompson',N=100,nysamples=100):
+        #untested
+        untested=[i for i in range(self.n) if STATUS[i]==0]
         if acquisition_function=='Thompson':
             alpha=self.samples()
         elif acquisition_function=='Greedy_N':
@@ -104,4 +128,4 @@ class prospector(object):
         else:
             alpha=np.random.rand(self.n)
         ipick=untested[np.argmax(alpha[untested])]
-        return ipick
+        return ipick,0
--- simulated_screed_HMOF.py
+++ simulated_screed_HMOF.py
@@ -29,11 +29,22 @@ y=Y[:,2]
 
 """ ignore any MOFs with nan values - or suspicious 2 with very high y """
 I=[i for i in range(n) if np.isnan(np.sum(X[i]+y[i]))==False and Y[i,2]<10]
-y=y[I]
+y_true_values=y[I].reshape(-1,1)
+
+""" status vector """
+STATUS=np.zeros((n,1))
+
+def return_y_value(i):
+    return y_true_values[i]
+
+Y=np.zeros((n,1))*np.nan
 X=X[I]
 
+n,d=X.shape
+
+
 """ true top 100 to compare sample with """
-top=np.argsort(y)[-100:]
+top=np.argsort(y_true_valuesy)[-100:]
 
 """ normalize features """
 n,d=X.shape
@@ -46,89 +57,105 @@ for j in range(d):
 """ sample  100 at random to start """
 p=np.random.permutation(n)        
 nrand=100
-tested=list(p[:nrand])
-untested=list(p[nrand:])
-P=BOGP.prospector()
-its_per_up=100
-ntested=nrand
+#tested=list(p[:nrand])
+#untested=list(p[nrand:])
+for i in range(nrand):
+    STATUS[p[i]]=2
+    Y[p[i]]=return_y_value(p[i])
+    
+    
+    
+P=BOGP.prospector(X)
 
+ntested=nrand
 """ lets go! """
 while ntested<2000:
 
-    """ update hyperparameters every 100 samples """
-    if np.mod(ntested,its_per_up)==0:
-        P.fit(X,untested,tested,y[tested])
-        """ estiamates number of top 100 found so far """
-        """ plots actual top 100 vs estimaed along with 90% range """
-        ny=100
-        YS=P.samples(nsamples=ny)
-        R=np.zeros((ntested,ny))
-        for j in range(ny):
-            topsapmled=np.argpartition(YS[:,j],-100)[-100:]
-            R[:,j]=np.cumsum([i in topsapmled for i in tested])
-        """ nice plot to show progress and estimates score """
-        plt.plot(np.cumsum([i in top for i in tested]),label='true top 100')
-        plt.plot(np.mean(R,1),label='estimated top 100')
-        plt.plot([ntested,ntested],[np.sort(R[-1])[4],np.sort(R[-1])[-5]])
-        plt.legend()
-        plt.xlabel('number of testes')
-        plt.ylabel('number of top 100 found')
-        plt.show()        
+    
+    P.fit(Y,STATUS)
+    
+#    
+#    """ update hyperparameters every 100 samples """
+#    if np.mod(ntested,its_per_up)==0:
+#        P.fit(X,untested,tested,y[tested])
+#        """ estiamates number of top 100 found so far """
+#        """ plots actual top 100 vs estimaed along with 90% range """
+#        ny=100
+#        YS=P.samples(nsamples=ny)
+#        R=np.zeros((ntested,ny))
+#        for j in range(ny):
+#            topsapmled=np.argpartition(YS[:,j],-100)[-100:]
+#            R[:,j]=np.cumsum([i in topsapmled for i in tested])
+#        """ nice plot to show progress and estimates score """
+#        plt.plot(np.cumsum([i in top for i in tested]),label='true top 100')
+#        plt.plot(np.mean(R,1),label='estimated top 100')
+#        plt.plot([ntested,ntested],[np.sort(R[-1])[4],np.sort(R[-1])[-5]])
+#        plt.legend()
+#        plt.xlabel('number of testes')
+#        plt.ylabel('number of top 100 found')
+#        plt.show()        
   
     """ sample next point """
-    i=P.pick_next(untested,acquisition_function='Thompson')
-    tested.append(i)
-    untested.remove(i)
-    """ update model """
-    P.update_model(tested,y[tested])
+    ipick,kpick=P.pick_next(STATUS)
+#    tested.append(i)
+#    untested.remove(i)
+    
+    """ show that we are testing ipick """
+    STATUS[ipick,kpick]=1
+    
+    """ now lets get the score and mark it as tested """
+    
+    Y[ipick,kpick]=return_y_value(ipick)
+    STATUS[ipick,kpick]=2
+    
     
     """ count sample and print out current score """
     ntested=ntested+1
     print(ntested)
-    print(sum(1 for i in tested if i in top))
+    print(sum(1 for i in range(n) if i in top and STATUS[i,0]==2))
 
-""" save list of tested MOFs """
-np.save('tested_thompson',tested)
-
-""" some nice plots """
-
-""" estiamates number of top 100 found so far """
-""" plots actual top 100 vs estimaed along with 90% range """
-ny=100
-YS=P.samples(nsamples=ny)
-R=np.zeros((ntested,ny))
-for j in range(ny):
-    topsapmled=np.argpartition(YS[:,j],-100)[-100:]
-    R[:,j]=np.cumsum([i in topsapmled for i in tested])
-""" nice plot to show progress and estimates score """
-plt.plot(np.cumsum([i in top for i in tested]),label='true top 100')
-plt.plot(np.mean(R,1),label='estimated top 100')
-plt.plot([ntested,ntested],[np.sort(R[-1])[4],np.sort(R[-1])[-5]])
-plt.legend()
-plt.xlabel('number of testes')
-plt.ylabel('number of top 100 found')
-plt.show()    
-
-""" breakdown into top 10, 25 etc... """
-top1000=np.argsort(y)[-1000:]
-N=[10,25,50,100,250,500]
-for j in range(6):
-    plt.plot(np.cumsum([i in top1000[-N[j]:] for i in tested])/N[j],label='top '+str(N[j]))
-plt.legend()
-plt.xlabel('number of testes')
-plt.ylabel('proportion found')
-plt.savefig('fig2.eps')
-plt.show()
-
-""" histogram of top MOFs found vs top MOFs in full database """
-y1000=y[top1000[0]]
-y100=y[top1000[-100]]
-H=np.histogram(y[y>y1000])
-plt.hist(y,H[1],label='full database')
-plt.hist(y[tested],H[1],label='AME sample')
-plt.plot([y100,y100],[0,np.max(H[0])],'--',color='black',label='top 100')
-plt.legend()
-plt.xlabel('API')
-plt.ylabel('frequency')
-plt.savefig('fig3.eps')
-plt.show()
+#""" save list of tested MOFs """
+#np.save('tested_thompson',tested)
+#
+#""" some nice plots """
+#
+#""" estiamates number of top 100 found so far """
+#""" plots actual top 100 vs estimaed along with 90% range """
+#ny=100
+#YS=P.samples(nsamples=ny)
+#R=np.zeros((ntested,ny))
+#for j in range(ny):
+#    topsapmled=np.argpartition(YS[:,j],-100)[-100:]
+#    R[:,j]=np.cumsum([i in topsapmled for i in tested])
+#""" nice plot to show progress and estimates score """
+#plt.plot(np.cumsum([i in top for i in tested]),label='true top 100')
+#plt.plot(np.mean(R,1),label='estimated top 100')
+#plt.plot([ntested,ntested],[np.sort(R[-1])[4],np.sort(R[-1])[-5]])
+#plt.legend()
+#plt.xlabel('number of testes')
+#plt.ylabel('number of top 100 found')
+#plt.show()    
+#
+#""" breakdown into top 10, 25 etc... """
+#top1000=np.argsort(y)[-1000:]
+#N=[10,25,50,100,250,500]
+#for j in range(6):
+#    plt.plot(np.cumsum([i in top1000[-N[j]:] for i in tested])/N[j],label='top '+str(N[j]))
+#plt.legend()
+#plt.xlabel('number of testes')
+#plt.ylabel('proportion found')
+#plt.savefig('fig2.eps')
+#plt.show()
+#
+#""" histogram of top MOFs found vs top MOFs in full database """
+#y1000=y[top1000[0]]
+#y100=y[top1000[-100]]
+#H=np.histogram(y[y>y1000])
+#plt.hist(y,H[1],label='full database')
+#plt.hist(y[tested],H[1],label='AME sample')
+#plt.plot([y100,y100],[0,np.max(H[0])],'--',color='black',label='top 100')
+#plt.legend()
+#plt.xlabel('API')
+#plt.ylabel('frequency')
+#plt.savefig('fig3.eps')
+#plt.show()

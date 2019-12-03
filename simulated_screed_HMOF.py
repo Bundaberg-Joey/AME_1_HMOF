3e6e61d84cd6ccfd0ea93dae0818850947d2b230
Calum Hand <crh53@bath.ac.uk>
2019-12-03 14:30:44 +0000

    added random seed generation

--- simulated_screed_HMOF.py
+++ simulated_screed_HMOF.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 
 import argparse
+import numpy as np
 
 import BOGP
 from AMI_Simulations import DataTriage, SimulatedScreener
@@ -15,6 +16,9 @@ if __name__ == '__main__':
     parser.add_argument('-i', '--initial_samples', action='store', default=100, help='# of random samples AMI takes')
     parser.add_argument('-m', '--max_iterations', action='store', default=2000, help='# of materials AMI will sample')
     args = parser.parse_args()
+
+    random_seed = np.random.randint(0, 2**32-1, 1)  # range of permissible values for Randomstate
+    np.random.RandomState(seed=random_seed)
 
     dt = DataTriage()
     X, y = dt.load_simulation_data(data_path=args.data_file, data_delimiter=',', headers_present=1)

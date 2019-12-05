18e10d24d481a1c8a5c980d5a3a31f66e36395f6
Calum Hand <crh53@bath.ac.uk>
2019-12-05 17:53:02 +0000

    change in datatriage object and reverting to serial run as per Gael's reccomendation

--- Simulated_AMI_Screen.py
+++ Simulated_AMI_Screen.py
@@ -1,31 +1,9 @@
 #!/usr/bin/env python3
 
 import argparse
-import multiprocessing
-from functools import partial
-from concurrent.futures import ProcessPoolExecutor
-
-import numpy as np
 
 import BOGP
-from AmiSimTools import DataTriage, SimulatedScreener
-
-
-def main(seed, simulator, model, num_initial_samples, verbose):
-    """
-    Allows the simulated screenings to be conducted in parallel, allowing for use on HPC systems.
-    The random state of each iteration is set by the passed seed and is updated in the screener meta data.
-
-    :param seed: numpy.int, integer value used to set the state of the random process for each parallel simulation
-    :param simulator: SimulatedScreener, object which performs the simulations with the model and updates meta data
-    :param model: prospector, AMI mathematical workhorse which is performing the material sampling and analysis
-    :param num_initial_samples: int, number of initial samples for the simulator to take before passing to AMI
-    :param verbose: boolean, if True, print output for user, if False then print nothing
-    :return: NoneType
-    """
-    np.random.RandomState(seed=seed)
-    simulator.initial_random_samples(num_initial_samples=num_initial_samples)
-    simulator.perform_screening(model=model, verbose=verbose)
+from AmiSimTools import DataTriageCSV, SimulatedScreener
 
 
 if __name__ == '__main__':
@@ -36,28 +14,17 @@ if __name__ == '__main__':
     parser.add_argument('-d', '--data_file', action='store', default=data_location, help='path to data file')
     parser.add_argument('-i', '--initial_samples', action='store', default=100, help='# of random samples AMI takes')
     parser.add_argument('-m', '--max_iterations', action='store', default=2000, help='# of materials AMI will sample')
-    parser.add_argument('-r', '--repetitions', action='store', default=0, help='# of repetitions to run per core')
     args = parser.parse_args()
 
-    dt = DataTriage()
-    X, y = dt.load_dataset_csv(data_path=args.data_file, data_delimiter=',', headers_present=1)
-    sim_params = dt.prepare_simulation_data(y=y)
+    sim_data = DataTriageCSV.load_from_path(args.data_file)
     # loads data from csv file and then determines `status` array and other parameters as dict needed for the screening
 
-    sim_screen = SimulatedScreener(simulation_params=sim_params, max_iterations=args.max_iterations)
-    ami = BOGP.prospector(X=X)
+    sim_screen = SimulatedScreener(data_params=sim_data, max_iterations=args.max_iterations)
+    ami = BOGP.prospector(X=sim_data.X)
     # initialises the AMI model and the simulation screener with the previously exported dict
 
-    n_cpus = multiprocessing.cpu_count()
-    num_scheduled_simulations = int(args.repetitions) * n_cpus if args.repetitions else 1
-    simulation_seeds = np.random.choice(2000000, num_scheduled_simulations, replace=False)  # sample without replacement
-    # determine the number of parallel process to run, if user flag is 0, only 1 will run
+    sim_screen.initial_random_samples(num_initial_samples=args.initial_samples)
+    sim_screen.perform_screening(model=ami, verbose=True)
+    # performs the screening of the loaded data set using the ami model after picking a number of initial samples
 
-    worker = partial(main, simulator=sim_screen, model=ami, num_initial_samples=args.initial_samples, verbose=True)
-
-    with ProcessPoolExecutor(max_workers=n_cpus) as pool:
-        for res in pool.map(worker, simulation_seeds):
-            pass
-    # perform multi thread processing of numerous AMI runs
-
-# TODO : add `update_metadata()` method to the SimulatedScreener class
+# TODO : have SimulatedScreener output data to a file

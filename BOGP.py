46d3d6faba927a5df3ea0c979b9c8319fa8df1f1
Calum Hand <crh53@bath.ac.uk>
2019-12-09 14:34:08 +0000

    adjusted comments and docstrings only!!!

--- BOGP.py
+++ BOGP.py
@@ -1,44 +1,53 @@
 #!/usr/bin/env python3
-# -*- coding: utf-8 -*-
-"""
-Created on Thu Oct 17 10:50:26 2019
 
-sparse GP for use in high dimensional large scale Bayesian Optimization problems
-fits hyperparameters using dense GPy model
-special routines for sparse sampling from posterior
-Thompson and Greedy_N acquisition functions 
-
-@author: Hook
 """
+Sparse GaussianProcess for use in high dimensional large scale Bayesian Optimization problems.
+Fits hyperparameters using dense GPy model.
+Special routines for sparse sampling from posterior.
+Thompson and Greedy_N acquisition functions.
+
+"""
+
+__author__ = 'James Hook'
+__version__ = '2.0.0'
 
 import numpy as np
 from sklearn.cluster import KMeans
 from sklearn.metrics.pairwise import euclidean_distances
+
 import GPy
+
 
 class prospector(object):
     
-    """ Initializes by storing all feature values """ 
+
     def __init__(self,X):
+        """ Initializes by storing all feature values """
         self.X=X
         self.n,self.d=X.shape
         self.update_counter=0
         self.updates_per_big_fit=10
     
-    """ fits hyperparameters and inducing points """
+
     def fit(self,Y,STATUS,ntop=100,nrecent=100,nmax=400,ntopmu=100,ntopvar=100,nkmeans=300,nkeamnsdata=5000,lam=1e-6):
+        """ fits hyperparameters and inducing points
+            fit a GPy dense model to get hyperparameters
+            take subsample for tested data for fitting
+            ntop top samples
+            nrecent most recent samples
+            random samples up to total of nmax
+            ntopmu most promising untested points
+            ntopvar most uncertain untested points
+            nkmeans cluster centers from untested data
+            lam controls jitter in g samples
+        """
+
         X=self.X
         untested=[i for i in range(self.n) if STATUS[i]==0]
-        #testing=[i for i in range(self.n) if STAUS[i]==1]
         tested=[i for i in range(self.n) if STATUS[i]==2]
         ytested=Y[tested].reshape(-1)
         if np.mod(self.update_counter,self.updates_per_big_fit)==0:
-            """ fit a GPy dense model to get hyperparameters """
-            """ take subsample for tested data for fitting """
-            """ ntop top samples """
-            """ nrecent most recent samples """
-            """ random samples up to total of nmax """
-            #self.n,self.d=X.shape
+
             print('fitting hyperparameters')
             ntested=len(tested)
             if ntested>nmax:
@@ -52,8 +61,8 @@ class prospector(object):
             else:
                 train=tested
                 ytrain=ytested
-            """ fit dense GPy model to this data """
-            mfy=GPy.mappings.Constant(input_dim=self.d,output_dim=1)
+
+            mfy=GPy.mappings.Constant(input_dim=self.d,output_dim=1)  # fit dense GPy model to this data
             ky = GPy.kern.RBF(self.d,ARD=True,lengthscale=np.ones(self.d))
             self.GP = GPy.models.GPRegression(X[train],ytrain.reshape(-1,1),kernel=ky,mean_function=mfy)
             self.GP.optimize('bfgs')
@@ -61,20 +70,17 @@ class prospector(object):
             self.a=self.GP.flattened_parameters[1]
             self.l=self.GP.flattened_parameters[2]
             self.b=self.GP.flattened_parameters[3]
-            """ now pick inducing points for sparse model """
-            """ use all the train points as above """
-            """ ntopmu most promising untested points """
-            """ ntopvar most uncertain untested points """
-            """ nkmeans cluster centers from untested data """
+            # now pick inducing points for sparse model, use all the train points as above
+
             print('selecting inducing points')
             self.py=self.GP.predict(X)
             topmu=[untested[i] for i in np.argsort(self.py[0][untested].reshape(-1))[-ntopmu:]]
             topvar=[untested[i] for i in np.argsort(self.py[1][untested].reshape(-1))[-ntopvar:]]
             nystrom=topmu+topvar+train
             kms=KMeans(n_clusters=nkmeans,max_iter=5).fit(np.divide(X[list(np.random.choice(untested,nkeamnsdata))],self.l)) 
+
             self.M=np.vstack((X[nystrom],np.multiply(kms.cluster_centers_,self.l)))
-            """ fit sparse model """
-            """ lam controls jitter in g samlpes """
+
             print('fitting sparse model')
             DXM=euclidean_distances(np.divide(X,self.l),np.divide(self.M,self.l),squared=True)
             self.SIG_XM=self.a*np.exp(-DXM/2)
@@ -91,32 +97,24 @@ class prospector(object):
             J=np.matmul(self.SIG_XM[tested].T,np.divide(ytested-self.mu,self.B[tested]))
             self.mu_M_pos=self.mu+J-np.matmul(K,np.linalg.solve(K+self.SIG_MM,J))            
         self.update_counter+=1
-        
-#    """ updates posterior on inducing points """
-#    def update_model(self,STATUS,Y):
-#        #tested,ytested
-#        tested=[i for i in range(self.n) if STATUS[i]==2]
-#        ytested=Y[tested]
-#        K=np.matmul(self.SIG_XM[tested].T,np.divide(self.SIG_XM[tested],self.B[tested].reshape(-1,1)))
-#        self.SIG_MM_pos=self.SIG_MM-K+np.matmul(K,np.linalg.solve(K+self.SIG_MM,K))
-#        J=np.matmul(self.SIG_XM[tested].T,np.divide(ytested-self.mu,self.B[tested]))
-#        self.mu_M_pos=self.mu+J-np.matmul(K,np.linalg.solve(K+self.SIG_MM,J))
 
-    """ get prediction on full dataset """        
+
     def predict(self):
+        """ get prediction on full dataset """
         mu_X_pos=self.mu+np.matmul(self.SIG_XM,np.linalg.solve(self.SIG_MM,self.mu_M_pos-self.mu))
         var_X_pos=np.sum(np.multiply(np.linalg.solve(self.SIG_MM_pos,self.SIG_XM.T),self.SIG_XM.T),0)
         return mu_X_pos,var_X_pos
-    
-    """ samples posterior on full dataset """
+
+
     def samples(self,nsamples=1):
+        """ samples posterior on full dataset """
         samples_M_pos=np.random.multivariate_normal(self.mu_M_pos,self.SIG_MM_pos,nsamples).T
         samples_X_pos=self.mu+np.matmul(self.SIG_XM,np.linalg.solve(self.SIG_MM,samples_M_pos-self.mu))
         return samples_X_pos
 
-    """ picks next point to sample """    
+
     def pick_next(self,STATUS,acquisition_function='Thompson',N=100,nysamples=100):
-        #untested
+        """ picks next point to sample """
         untested=[i for i in range(self.n) if STATUS[i]==0]
         if acquisition_function=='Thompson':
             alpha=self.samples()

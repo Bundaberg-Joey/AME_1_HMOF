0dc81e9d245da64355670c55f5b66e979f19ad5a
Calum Hand <crh53@bath.ac.uk>
2020-02-13 18:25:03 +0000

    branching for 500 initial samples and stopping. Also included score function to make life easier in post processing.

--- Simulated_AMI_Screen.py
+++ Simulated_AMI_Screen.py
@@ -1,11 +1,38 @@
 #!/usr/bin/env python3
 
 import argparse
+import json
+from uuid import uuid4
+
+import numpy as np
+import pandas as pd
 
 import BOGP
-
 from AmiSimTools.DataTriage import DataTriageCSV
 from AmiSimTools.SimScreen import SimulatedScreenerSerial
+
+
+def prediction_score(y, z):
+    """
+    calculate difference between ame predictions and true scores
+    :param y: true scores
+    :param z: predicted scores
+    :return: difference
+    """
+    y_bar = y.mean()
+    score = np.sqrt(np.sum(np.power(y-z, 2)) / np.sum(np.power(y-y_bar, 2)))
+    return score
+
+
+def save_data(data):
+    """
+    save data to json file, with uuid 4 named output file
+    :param data: dict
+    """
+    file_id = str(uuid4())
+    with open(F'{file_id}.json', 'w') as f:
+        f.write(json.dumps(data, indent=4))
+
 
 
 if __name__ == '__main__':
@@ -14,20 +41,33 @@ if __name__ == '__main__':
 
     parser = argparse.ArgumentParser()
     parser.add_argument('-d', '--data_file', action='store', default=data_location, help='path to data file')
-    parser.add_argument('-i', '--initial_samples', action='store', type=int, default=100, help='# of random samples AMI takes')
-    parser.add_argument('-m', '--max_iterations', action='store', type=int, default=2000, help='# of materials AMI will sample')
-    parser.add_argument('-s', '--sim_code', action='store', type=str, default='N/A', help='Reference code for simulation')
+    parser.add_argument('-i', '--initial_samples', action='store', type=int, default=500, help='# of random samples AMI takes')
     parser.add_argument('-a', '--acquisition', action='store', type=str, default='Thompson', help='AMI acquisition func')
     args = parser.parse_args()
 
     sim_data = DataTriageCSV.load_from_path(args.data_file)
     # loads data from csv file and then determines `status` array and other parameters as dict needed for the screening
 
-    sim_screen = SimulatedScreenerSerial(data_params=sim_data, max_iterations=args.max_iterations, sim_code=args.sim_code)
+    sim_screen = SimulatedScreenerSerial(data_params=sim_data, max_iterations=501, sim_code='N/A')
     ami = BOGP.prospector(X=sim_data.X, acquisition_function=args.acquisition)
     # initialises the AMI model and the simulation screener with the triaged data
 
     sim_screen.initial_random_samples(num_initial_samples=args.initial_samples)
-    sim_screen.perform_screening(model=ami, verbose=True)
-    # performs the screening of the loaded data set using the ami model after picking a number of initial samples
+    z_mu, z_var = sim_screen.perform_screening(model=ami)
+    ame_score = prediction_score(sim_data.y, z_mu)  # calc score needed for test
+
+    columns = pd.read_csv(args.data_file).columns
+    features, target = list(columns[:-1]), str(columns[-1])
+
+    material, feat_code, bad_target = args.data_file.split('_')
+
+    results_dict = {'features': features,
+                    'target': target,
+                    'ami_score': ame_score,
+                    'data_set': args.data_file,
+                    'feature_code': feat_code,
+                    'material': material}
+
+    save_data(results_dict)
+
 

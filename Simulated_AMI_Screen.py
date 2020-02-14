ee7faf7b2f97ac13109fd92c546f7056b6a83cd4
Calum Hand <crh53@bath.ac.uk>
2020-02-14 13:29:36 +0000

    updatinf saved output to pickle so can include output numpy arrays

--- Simulated_AMI_Screen.py
+++ Simulated_AMI_Screen.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 
-import json
+import pickle
 from uuid import uuid4
 import argparse
 
@@ -26,18 +26,19 @@ def prediction_score(y, z):
 
 def save_data(data):
     """
-    save data to json file, with uuid 4 named output file
+    save data to pickle file, with uuid 4 named output file
     :param data: dict
     """
     file_id = str(uuid4())
-    with open(F'json_output/{file_id}.json', 'w') as f:
-        f.write(json.dumps(data, indent=4))
+    with open(F'pickle_output/{file_id}.pkl', 'wb') as f:
+        pickle.dump(data, f)
 
 
 if __name__ == '__main__':
 
     parser = argparse.ArgumentParser()
     parser.add_argument('-d', '--data_file', action='store', default='', help='path to data file')
+    parser.add_argument('-n', '--num_samples', action='store', type=int, default=350, help='num random samples')
     args = parser.parse_args()
 
     sim_data = DataTriageCSV.load_from_path(args.data_file)  # load data and format
@@ -46,7 +47,7 @@ if __name__ == '__main__':
     ami = BOGP.prospector(X=sim_data.X, acquisition_function='Thompson')
     # initialises the AMI model and the simulation screener with the triaged data
 
-    sim_screen.initial_random_samples(num_initial_samples=500)  # sample 500 materials and then do killswitch
+    sim_screen.initial_random_samples(num_initial_samples=args.num_samples)  # sample materials and then do killswitch
     z_mu, z_var = sim_screen.perform_killswitch_screen(model=ami)
 
     ame_score = prediction_score(sim_data.y, z_mu)  # calc score needed for test
@@ -61,6 +62,10 @@ if __name__ == '__main__':
                     'ami_score': ame_score,
                     'data_set': args.data_file,
                     'feature_code': feat_code,
-                    'material': material}
+                    'material': material,
+                    'true': sim_data.y,
+                    'z_mu': z_mu,
+                    'z_var': z_var,
+                    'random_samples': args.num_samples}
 
     save_data(results_dict)

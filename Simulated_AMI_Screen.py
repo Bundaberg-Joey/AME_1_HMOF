1fe4702b17bb1715d5349cba2302677b58592ead
Calum Hand <crh53@bath.ac.uk>
2020-03-31 11:43:35 +0100

    Significantly updated the doccumentation (and capitalisation) of the `Prospector` class.

--- Simulated_AMI_Screen.py
+++ Simulated_AMI_Screen.py
@@ -25,7 +25,7 @@ if __name__ == '__main__':
     # loads data from csv file and then determines `status` array and other parameters as dict needed for the screening
 
     sim_screen = SimulatedScreenerSerial(data_params=sim_data, max_iterations=args.max_iterations, sim_code=args.sim_code)
-    ami = BOGP.prospector(X=sim_data.X, acquisition_function=args.acquisition)
+    ami = BOGP.Prospector(X=sim_data.X, acquisition_function=args.acquisition)
     # initialises the AMI model and the simulation screener with the triaged data
 
     sim_screen.initial_random_samples(num_initial_samples=args.initial_samples)

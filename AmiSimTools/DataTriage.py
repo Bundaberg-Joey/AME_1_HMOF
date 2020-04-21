894ea866d3707afa869f5e4d21187c500f8c1a96
Calum Hand <crh53@bath.ac.uk>
2020-04-21 18:50:07 +0100

    Updated DataTriage doccumentation and overall strcture. Have inlcuded multiple checks of the data on initialisation to go full agro on the defensive programming.

--- AmiSimTools/DataTriage.py
+++ AmiSimTools/DataTriage.py
@@ -1,95 +1,133 @@
-
 """
-This file contains classes used to load and handle the data in order for it to be read from file and into the AMI
+Contains classes used to help the loading and preprocessing of data for screening.
+Data can be processed straight from numpy arrays OR through provision of a csv file / mat lab file path.
 """
-
-__author__ = 'Calum Hand'
-__version__ = '1.0.0'
-
-import warnings
 
 import numpy as np
+import pandas as pd
 from scipy.io import loadmat
+
+from ami import checks
 
 
 class DataTriage(object):
-    """
-    This Parent takes a dataset which is to be assessed by the AMI and then performs the necessary pre-processing
-    steps on the data set including: loading the data into numpy arrays, formatting the target values into the correct
-    representation and other values.
+    """Processes feature and target information into formats required by the screening.
 
-    Children of this class allow for multiple types of files to be loaded and used depending on user requirements
+    Attributes
+    ----------
+    X : np.array(), shape(num_entries, num_features)
+        Feature matrix.
+
+    y_true : np.array(), shape(num_entries, )
+        Target values.
+
+    y_experimental : np.array(), shape(num_entries, )
+        Array prepopulated with `Nan` values, to be updated as screening proceeds.
+
+    top_100 : np.array(), shape(100_top_entries, )
+        Array containing indices of the 100 data points with highest target values.
+
+    Methods
+    -------
+    load_from_path(cls, data_path) --> Not implemented in Parent class.
     """
 
     def __init__(self, X, y):
-        assert len(X) == len(y)
-        self.X = X
-        self.y = y
-        self.n = len(self.y)
-        self.y_true, self.y_experimental = self.format_target_values(self.y, self.n)
-        self.status = np.zeros((self.n, 1))
-        self.top_100 = np.argsort(self.y)[-100:]
+        """Checks arrays are not empty, are the same length and have no nan values.
 
+        Parameters
+        ----------
+        X : np.array(), shape(num_entries, num_features)
+            Feature matrix.
+
+        y : np.array(), shape(num_entries, )
+            Target values.
+        """
+        X, y = np.array(X), np.array(y)
+        checks.array_empty(X, y)
+        checks.nan_present(X, y)
+
+        if len(X) != len(y):
+            raise ValueError('Lengths of feature and target array do not match')
+
+        self.X = X
+        self.y_true, self.y_experimental = self.format_target_values(y)
+        self.top_100 = np.argsort(self.y_true)[-100:]
 
     @staticmethod
     def _load_dataset_from_path(data_path):
-        """
-        Method to be overloaded in further subclasses, loads data for the object
+        """Method to be overloaded in further subclasses, loads data for the object.
 
-        :param data_path: str, path to data
-        :return: x, y: (np.ndarray, np.ndarray)
+        Parameters
+        ----------
+        data_path : str
+            Path to file containing data.
+
+        Returns
+        -------
+        NotImplemented
         """
         return NotImplemented
 
-
     @classmethod
     def load_from_path(cls, data_path):
-        """
-        load data from the overloaded class method based on the location and then instantiate the object
+        """Factory method for class when loading data from file.
 
-        :param data_path: str, path to data
-        :return: object, instantiated DataTriage object
+        Parameters
+        ----------
+        data_path : str
+            Path to file containing data.
+
+        Returns
+        -------
+        cls(X, y) : DataTriage
+            Class method creates DataTriage object.
         """
+
         X, y = cls._load_dataset_from_path(data_path)
         return cls(X, y)
 
-
     @staticmethod
-    def format_target_values(y, n):
-        """
-        For simulated screenings, AMI requires an experimental column of results it has determined itself and the
-        "True" target values which it uses to evaluate chosen materials against. These must be in the correct
-        matrix / vector shape.
+    def format_target_values(y):
+        """Transform target values into array of "known" values and a separate array containing empirical results.
+        Empirical array is prepopulated with `nan` values to avoid confusion with target values equal to 0.
 
-        :param y: np.array(), size `n` array containing the loaded target values
-        :param n: int, the number of entries in the passed array `y`
-        :return: (y_true, y_experimental), column vectors, [0] with all target values, [1] for determined values
+        Parameters
+        ----------
+        y : np.array(), shape(num_entries, )
+            Target values.
+
+        Returns
+        -------
+        (y_true, y_experimental) : (np.array(), np.array()), shape((num_entries, ), (num_entries, ))
         """
         y_true = y
-        y_experimental = np.full(n, np.nan)  # nan as values not yet determined on initialisation
+        y_experimental = np.full(len(y), np.nan)
         return y_true, y_experimental
 
 
 class DataTriageCSV(DataTriage):
+    """Child of DataTriage which facilitates loading data from a csv file.
     """
-    Child class which allows for the loading of data from a csv file
-    """
-    @staticmethod
-    def _load_dataset_from_path(path: str) -> (np.ndarray, np.ndarray):
-        """
-        Loads the features and target variables for the AME to assess from a delimited file, assumed csv.
-        The default loading removes the first row to allow headed files to be read and so should be specified if not.
-        The delimited file is assumed to be structured with target values as the final right hand column.
 
-        :param path: str, location of the csv data file to be read
-        :return: features: np.array(), `m` by 'n' array which is the feature matrix of the data being modelled
-        :return: targets: np.array(), `m` sized array containing the target values for the passed features
+    @staticmethod
+    def _load_dataset_from_path(path):
+        """Loads data from csv file, assumed target column is rightmost column of the file.
+        Uses pandas to load the data so multiple delimiter types are supported.
+
+        Parameters
+        ----------
+        path : str
+            Path to csv file.
+
+        Returns
+        -------
+        (X, y) : (np.array(), np.array(), shape((num_entries, nun_features), (num_entries, ))
+            Feature and target value arrays.
         """
-        data_set = np.loadtxt(path, delimiter=",", skiprows=1, dtype='float')
-        if data_set.size <= 0:
-            warnings.warn('Loaded data set was empty')
-        features, targets = data_set[:, :-1], data_set[:, -1]
-        return features, targets
+        data = pd.read_csv(path, dtype='float').to_numpy()
+        X, y = data[:, :-1], data[:, -1]
+        return X, y
 
 
 class DataTriageMatlab(DataTriage):
@@ -98,22 +136,18 @@ class DataTriageMatlab(DataTriage):
     """
     @staticmethod
     def _load_dataset_from_path(path):
-        """
-        Loads the feature and target variables from a matlab file where the feature matrix and target array are in
-        separate keys. The size of both the feature matrix and arrays are assessed and a warning is issued
-        if the arrays are empty.
+        """Loads data from matlab file, assumed that feature matrix is located at `X` and target values at `y`.
 
-        :param path: str, location of the matlab file to be read
-        :return: features: np.array(), `m` by 'n' array which is the feature matrix of the data being modelled
-        :return: targets: np.array(), `m` sized array containing the target values for the passed features
+        Parameters
+        ----------
+        path : str
+            Path to matlab file.
+
+        Returns
+        -------
+        (X, y) : (np.array(), np.array(), shape((num_entries, nun_features), (num_entries, ))
+            Feature and target value arrays.
         """
         data_set = loadmat(path, appendmat=False)
-        feature_key, target_key = 'X', 'y'
-        features, targets = data_set[feature_key], data_set[target_key]
-        if features.size <= 0 or targets.size <= 0:
-            warnings.warn('Loaded feature matrix or target array was empty')
-        if features.shape[0] != targets.shape[0]:
-            warnings.warn('The number of entries in the feature matrix and target array do not match')
+        features, targets = data_set['X'], data_set['y']
         return features, targets
-
-########################################################################################################################

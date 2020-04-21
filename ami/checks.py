f083b5e8aa80ca73e6237edca03c4831b2bc79f4
Calum Hand <crh53@bath.ac.uk>
2020-04-21 18:49:03 +0100

    Added checks to check for presence of `Nan` values or empty arrays.

--- ami/checks.py
+++ ami/checks.py
@@ -4,6 +4,7 @@ Functions ensure correct type and value of values are pased.
 Used for setters, etc.
 """
 
+import numpy as np
 
 def pos_int(value):
     """checks if valus is positive integer and returns if true.
@@ -43,3 +44,39 @@ def any_float(value):
     if not isinstance(value, float):
         raise TypeError('Value must be a float')
     return value
+
+
+def nan_present(*args):
+    """Checks for the presence of `Nan` values in numpy arrays.
+    Raises `ValueError` is present.
+
+    Parameters
+    ----------
+    args : np.array(), shape(num_entries, num_features)
+        Array(s) to check for presence of nan
+
+    Returns
+    -------
+    None
+    """
+    for a in args:
+        if np.isnan(a).sum() > 0:
+            raise ValueError('Nan values must not be present in array')
+
+
+def array_empty(*args):
+    """Checks if passed numpy array(s) are empty or not.
+    Raises `ValueError` if empty.
+
+    Parameters
+    ----------
+    args : np.array(), shape(num_entries, num_features)
+        Array(s) to check for presence of nan
+
+    Returns
+    -------
+    None
+    """
+    for a in args:
+        if a.size == 0:
+            raise ValueError('Array is empty')
